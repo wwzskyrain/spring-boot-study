1da227646d8e6d5d3c8f732abea3108c04536bd5
erik.wang <erik.wang@ximalaya.com>
2019-12-16 09:32:02 +0800

    add swagger demo

---
+++ spring-boot-swagger-demo/src/main/doc/ReadMe.md
@@ -0,0 +1,2 @@
+# 1.基本操作
+1.  [swagger主页](http://localhost:8080/swagger2-demo/swagger-ui.html)
---
+++ spring-boot-swagger-demo/src/main/java/erik/study/spring/boot/swagger/SpringBootSwaggerDemoApplication.java
@@ -0,0 +1,13 @@
+package erik.study.spring.boot.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwaggerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwaggerDemoApplication.class, args);
+	}
+
+}
